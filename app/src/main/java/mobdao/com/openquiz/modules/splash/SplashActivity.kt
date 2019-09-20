40731d979a0b9c4fc36ab4a1c2f875c5b662f3d7
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-09-20 10:32:03 -0300

    Feat: send route destination from splash to main

--- app/src/main/java/mobdao/com/openquiz/modules/splash/SplashActivity.kt
+++ app/src/main/java/mobdao/com/openquiz/modules/splash/SplashActivity.kt
@@ -1,8 +1,12 @@
 package mobdao.com.openquiz.modules.splash
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import mobdao.com.openquiz.modules.main.MainActivity
+import mobdao.com.openquiz.utils.constants.IntentConstants.INITIAL_SCREEN
+import mobdao.com.openquiz.utils.enums.InitialScreenType.HOME
+import mobdao.com.openquiz.utils.enums.InitialScreenType.LOGIN
 import mobdao.com.openquiz.utils.extensions.getViewModel
 import mobdao.com.openquiz.utils.extensions.setupObserver
 
@@ -25,11 +29,11 @@ class SplashActivity : AppCompatActivity() {
 
     private fun setupObservers() = with(viewModel) {
         setupObserver(isUserLoggedInLiveData to { isUserLoggedIn ->
-            // TODO route to correct screen
-            if (isUserLoggedIn) {
-                Toast.makeText(this@SplashActivity, "is logged in", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this@SplashActivity, "is not logged in", Toast.LENGTH_SHORT).show()
+            val initialScreenType = if (isUserLoggedIn) HOME else LOGIN
+            Intent(this@SplashActivity, MainActivity::class.java).apply {
+                putExtra(INITIAL_SCREEN, initialScreenType)
+                startActivity(this)
+                finish()
             }
         })
     }
