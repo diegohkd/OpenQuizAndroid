aa800588ee097e3fde964db35a4eac9c90f040bd
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-09-20 13:38:48 -0300

    Feat: inject splash view model and UserAuthRepository

--- app/src/main/java/mobdao/com/openquiz/data/repositories/UserAuthRepository.kt
+++ app/src/main/java/mobdao/com/openquiz/data/repositories/UserAuthRepository.kt
@@ -3,8 +3,9 @@ package mobdao.com.openquiz.data.repositories
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
+import javax.inject.Inject
 
-class UserAuthRepository {
+class UserAuthRepository @Inject constructor() {
 
     fun isUserLoggedIn(): LiveData<Boolean> =
         MutableLiveData<Boolean>().apply {
--- app/src/main/java/mobdao/com/openquiz/modules/splash/SplashActivity.kt
+++ app/src/main/java/mobdao/com/openquiz/modules/splash/SplashActivity.kt
@@ -2,17 +2,26 @@ package mobdao.com.openquiz.modules.splash
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import mobdao.com.openquiz.di.components.DaggerSplashComponent
 import mobdao.com.openquiz.modules.main.MainActivity
 import mobdao.com.openquiz.utils.constants.IntentConstants.INITIAL_SCREEN
+import mobdao.com.openquiz.utils.enums.InitialScreenType
 import mobdao.com.openquiz.utils.enums.InitialScreenType.HOME
 import mobdao.com.openquiz.utils.enums.InitialScreenType.LOGIN
-import mobdao.com.openquiz.utils.extensions.getViewModel
 import mobdao.com.openquiz.utils.extensions.setupObserver
+import javax.inject.Inject
 
 class SplashActivity : AppCompatActivity() {
 
-    private val viewModel: SplashViewModel by lazy(::getViewModel)
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+        @Inject set
+
+    private val viewModel: SplashViewModel by viewModels {
+        viewModelFactory
+    }
 
     //region Lifecycle
 
@@ -20,6 +29,7 @@ class SplashActivity : AppCompatActivity() {
         savedInstanceState: Bundle?
     ) {
         super.onCreate(savedInstanceState)
+        DaggerSplashComponent.create().inject(this)
         setupObservers()
     }
 
@@ -30,12 +40,16 @@ class SplashActivity : AppCompatActivity() {
     private fun setupObservers() = with(viewModel) {
         setupObserver(isUserLoggedInLiveData to { isUserLoggedIn ->
             val initialScreenType = if (isUserLoggedIn) HOME else LOGIN
-            Intent(this@SplashActivity, MainActivity::class.java).apply {
-                putExtra(INITIAL_SCREEN, initialScreenType)
-                startActivity(this)
-                finish()
-            }
+            showMain(initialScreenType)
         })
+    }
+
+    private fun showMain(initialScreenType: InitialScreenType) {
+        Intent(this@SplashActivity, MainActivity::class.java).apply {
+            putExtra(INITIAL_SCREEN, initialScreenType)
+            startActivity(this)
+            finish()
+        }
     }
 
     //endregion
