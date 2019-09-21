36904189c0201821926fbc1bc3344861af1f9bf2
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-09-21 17:03:29 -0300

    Feat: add LoginViewModel

--- app/src/main/java/mobdao/com/openquiz/di/modules/ViewModelModule.kt
+++ app/src/main/java/mobdao/com/openquiz/di/modules/ViewModelModule.kt
@@ -6,17 +6,24 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 import mobdao.com.openquiz.di.keys.ViewModelKey
+import mobdao.com.openquiz.modules.login.LoginViewModel
 import mobdao.com.openquiz.modules.splash.SplashViewModel
 import mobdao.com.openquiz.utils.factories.ViewModelFactory
 
 @Suppress("unused")
 @Module
 abstract class ViewModelModule {
-    @Binds
-    @IntoMap
-    @ViewModelKey(SplashViewModel::class)
-    abstract fun bindUserViewModel(splashViewModel: SplashViewModel): ViewModel
 
     @Binds
     abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SplashViewModel::class)
+    abstract fun bindSplashViewModel(splashViewModel: SplashViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(LoginViewModel::class)
+    abstract fun bindLoginViewModel(loginViewModel: LoginViewModel): ViewModel
 }
