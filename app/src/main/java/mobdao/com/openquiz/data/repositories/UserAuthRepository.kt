cba75a129f7a9c7cbefc9a5f6653eaa47ccc81be
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-09-21 14:46:24 -0300

    Feat: add login on Firebase to repository

--- app/src/main/java/mobdao/com/openquiz/data/repositories/UserAuthRepository.kt
+++ app/src/main/java/mobdao/com/openquiz/data/repositories/UserAuthRepository.kt
@@ -2,6 +2,7 @@ package mobdao.com.openquiz.data.repositories
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import javax.inject.Inject
@@ -16,4 +17,20 @@ class UserAuthRepository @Inject constructor(
         MutableLiveData<Boolean>().apply {
             postValue(FirebaseAuth.getInstance().currentUser != null)
         }
+
+    fun loginOnFirebase(
+        account: GoogleSignInAccount,
+        success: () -> Unit,
+        failure: (Exception?) -> Unit
+    ) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    success()
+                } else {
+                    failure(task.exception)
+                }
+            }
+    }
 }
