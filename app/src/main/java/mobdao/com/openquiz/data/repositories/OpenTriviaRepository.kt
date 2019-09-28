26e67cd6bf5cea019a68eaf203489bdc63602942
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-09-28 14:25:04 -0300

    Feat: add request to get open trivia token

--- app/src/main/java/mobdao/com/openquiz/data/repositories/OpenTriviaRepository.kt
+++ app/src/main/java/mobdao/com/openquiz/data/repositories/OpenTriviaRepository.kt
@@ -1,11 +1,20 @@
 package mobdao.com.openquiz.data.repositories
 
+import mobdao.com.openquiz.data.server.responses.SessionTokenResponse
+import mobdao.com.openquiz.data.server.webservices.SessionTokenService
+import mobdao.com.openquiz.data.utils.Single
+import mobdao.com.openquiz.data.utils.toSingle
 import retrofit2.Retrofit
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class OpenTriviaRepository @Inject constructor(
-    val retrofit: Retrofit
+    private val retrofit: Retrofit
 ) {
+
+    fun fetchSessionToken(): Single<SessionTokenResponse> =
+        retrofit.create(SessionTokenService::class.java)
+            .fetchSessionToken()
+            .toSingle()
 }
---
+++ app/src/main/java/mobdao/com/openquiz/data/server/responses/SessionTokenResponse.kt
@@ -0,0 +1,5 @@
+package mobdao.com.openquiz.data.server.responses
+
+data class SessionTokenResponse(
+    val token: String? = null
+)
