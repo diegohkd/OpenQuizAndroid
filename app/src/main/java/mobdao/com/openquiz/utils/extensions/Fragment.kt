96511b7d8e3391aa7b6169f3c31800351ee49426
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-10-03 13:28:22 -0300

    Feat: add Fragment+ViewModel extensions

--- app/src/main/java/mobdao/com/openquiz/utils/extensions/Fragment.kt
+++ app/src/main/java/mobdao/com/openquiz/utils/extensions/Fragment.kt
@@ -1,9 +1,14 @@
 package mobdao.com.openquiz.utils.extensions
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import mobdao.com.openquiz.modules.base.BaseFragment
 
-fun BaseFragment.setupSingleEventObserver(
+fun <T> Fragment.setupObserver(
+    pair: Pair<LiveData<T>, (T) -> Unit>
+) = pair.first.observe(viewLifecycleOwner, Observer { it?.let(pair.second) })
+
+fun Fragment.setupSingleEventObserver(
     pair: Pair<MutableLiveData<Unit>, () -> Unit>
-) = pair.first.observe(this, Observer { pair.second() })
+) = pair.first.observe(viewLifecycleOwner, Observer { pair.second() })
