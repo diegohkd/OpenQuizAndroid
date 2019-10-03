157e9ab5bfc0fa09d8169bc0b85da2f509ffc684
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-10-03 13:25:19 -0300

    Feat: add BaseViewModule

--- app/src/main/java/mobdao/com/openquiz/modules/login/LoginViewModel.kt
+++ app/src/main/java/mobdao/com/openquiz/modules/login/LoginViewModel.kt
@@ -2,19 +2,19 @@ package mobdao.com.openquiz.modules.login
 
 import android.app.Activity
 import android.content.Intent
-import androidx.lifecycle.ViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import mobdao.com.openquiz.data.repositories.UserAuthRepository
+import mobdao.com.openquiz.modules.base.BaseViewModel
 import mobdao.com.openquiz.utils.constants.RequestCodeConstants
 import mobdao.com.openquiz.utils.livedata.SingleLiveEvent
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(
     private val userAuthRepository: UserAuthRepository
-) : ViewModel() {
+) : BaseViewModel() {
 
     val showHomeScreenEvent: SingleLiveEvent<Unit> = SingleLiveEvent()
     val errorEvent: SingleLiveEvent<Unit> = SingleLiveEvent()
