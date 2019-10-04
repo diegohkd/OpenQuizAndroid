dc17f6f8164e8e7bb6b96d9e0db7aa3f5f0e356b
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-10-04 10:28:02 -0300

    Feat: add func to fetch questions from repository

--- data/src/main/java/mobdao/com/openquiz/data/repositories/OpenTriviaRepository.kt
+++ data/src/main/java/mobdao/com/openquiz/data/repositories/OpenTriviaRepository.kt
@@ -1,15 +1,19 @@
 package mobdao.com.openquiz.data.repositories
 
 import mobdao.com.openquiz.data.di.scopes.DataSingleton
+import mobdao.com.openquiz.data.server.mappers.QuestionServiceMapper
+import mobdao.com.openquiz.data.server.webservices.QuestionsService
 import mobdao.com.openquiz.data.server.webservices.SessionTokenService
-import mobdao.com.openquiz.data.utils.singles.Single
 import mobdao.com.openquiz.data.utils.extensions.toSingle
+import mobdao.com.openquiz.data.utils.singles.Single
+import mobdao.com.openquiz.models.Question
 import retrofit2.Retrofit
 import javax.inject.Inject
 
 @DataSingleton
 class OpenTriviaRepository @Inject constructor(
-    private val retrofit: Retrofit
+    private val retrofit: Retrofit,
+    private val questionServiceMapper: QuestionServiceMapper
 ) {
 
     fun fetchSessionToken(): Single<String> =
@@ -18,5 +22,16 @@ class OpenTriviaRepository @Inject constructor(
             .toSingle()
             .flatMap { sessionToken ->
                 Single.just(sessionToken.token.orEmpty())
+            }
+
+    fun fetchQuestions(nOfQuestions: Int): Single<List<Question>> =
+        retrofit.create(QuestionsService::class.java)
+            .fetchQuestions(nOfQuestions)
+            .toSingle()
+            .flatMap { response ->
+                Single.just(
+                    // TODO handle response.response_code
+                    questionServiceMapper.questionResponseToModel(response)
+                )
             }
 }
