d68e94189d0aeaca61cac947a53ab948e32c0666
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-10-05 21:11:31 -0300

    Feat: add non swipeable view pager

---
+++ uicomponents/src/main/java/mobdao/com/openquiz/uicomponents/NonSwipeableViewPager.kt
@@ -0,0 +1,18 @@
+package mobdao.com.openquiz.uicomponents
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+@Suppress("UNUSED_PARAMETER")
+class NonSwipeableViewPager @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ViewPager(context, attrs) {
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean = false
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean = false
+}
