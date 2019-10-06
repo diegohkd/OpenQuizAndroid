497a68785ad70766ee12cfcc4703125af401d37e
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2019-10-05 21:20:15 -0300

    Feat: add logic to start quiz

--- app/src/main/java/mobdao/com/openquiz/modules/home/HomeViewModel.kt
+++ app/src/main/java/mobdao/com/openquiz/modules/home/HomeViewModel.kt
@@ -1,29 +1,36 @@
 package mobdao.com.openquiz.modules.home
 
-import android.util.Log
 import mobdao.com.openquiz.data.repositories.OpenTriviaRepository
 import mobdao.com.openquiz.data.repositories.UserAuthRepository
 import mobdao.com.openquiz.data.utils.callbacks.Callback
 import mobdao.com.openquiz.data.utils.disposables.Disposable
+import mobdao.com.openquiz.models.Question
 import mobdao.com.openquiz.modules.base.BaseViewModel
 import mobdao.com.openquiz.utils.livedata.SingleLiveEvent
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
     private val userAuthRepository: UserAuthRepository,
-    openTriviaRepository: OpenTriviaRepository
+    private val openTriviaRepository: OpenTriviaRepository
 ) : BaseViewModel() {
 
+    val startQuizEvent: SingleLiveEvent<List<Question>> = SingleLiveEvent()
     val signOutEvent: SingleLiveEvent<Unit> = SingleLiveEvent()
-    var disposable: Disposable? = null
 
-    init {
-        disposable = openTriviaRepository.fetchSessionToken()
+    private var nOfQuestions = 10
+    private var disposable: Disposable? = null
+
+    fun onStartQuizClicked() {
+        showProgressBar()
+        disposable = openTriviaRepository.fetchQuestions(nOfQuestions)
             .subscribeBy(
-                Callback({ result ->
-                    Log.d("HomeViewModel", "token: $result")
+                Callback({ questions ->
+                    hideProgressBar()
+                    startQuizEvent.postValue(questions)
                 }, { exception ->
+                    // TODO handle error
                     exception?.printStackTrace()
+                    hideProgressBar()
                 })
             )
     }
