26957f5486de1a849834d51f6dda0b770cee9369
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2020-04-21 22:19:32 -0300

    Refactor: remove no longer used single

--- app/src/main/java/mobdao/com/openquiz/di/components/HomeComponent.kt
+++ app/src/main/java/mobdao/com/openquiz/di/components/HomeComponent.kt
@@ -2,15 +2,12 @@ package mobdao.com.openquiz.di.components
 
 import dagger.Component
 import mobdao.com.openquiz.data.di.components.DataComponent
-import mobdao.com.openquiz.di.modules.DisposableModule
 import mobdao.com.openquiz.di.modules.HomeViewModelModule
 import mobdao.com.openquiz.di.scopes.AppSingleton
 import mobdao.com.openquiz.modules.home.HomeFragment
 
 @Component(
-    modules = [HomeViewModelModule::class,
-        DisposableModule::class
-    ],
+    modules = [HomeViewModelModule::class],
     dependencies = [DataComponent::class]
 )
 @AppSingleton
