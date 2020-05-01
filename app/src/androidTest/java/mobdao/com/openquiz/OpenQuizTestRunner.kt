9de3dcca24e4f166ba0055c892eaa0fd3d0cb104
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2020-05-01 19:04:27 -0300

    Refactor: UI tests

---
+++ app/src/androidTest/java/mobdao/com/openquiz/OpenQuizTestRunner.kt
@@ -0,0 +1,14 @@
+package mobdao.com.openquiz
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+
+/**
+ * Custom runner to disable dependency injection.
+ */
+class OpenQuizTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
+        return super.newApplication(cl, TestApp::class.java.name, context)
+    }
+}
