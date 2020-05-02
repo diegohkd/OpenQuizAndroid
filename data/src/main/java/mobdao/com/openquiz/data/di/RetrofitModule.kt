b9b768869b86b065eaffd1ebba620662b5aa8bd4
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2020-05-02 16:30:40 -0300

    Feat: inject http interceptor only for debug variant

--- data/src/main/java/mobdao/com/openquiz/data/di/RetrofitModule.kt
+++ data/src/main/java/mobdao/com/openquiz/data/di/RetrofitModule.kt
@@ -1,11 +1,8 @@
 package mobdao.com.openquiz.data.di
 
-import android.util.Log
 import com.google.gson.GsonBuilder
-import mobdao.com.openquiz.data.BuildConfig
 import mobdao.com.openquiz.data.utils.constants.Constants.BASE_OPEN_TRIVIA_URL
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,28 +11,13 @@ val retrofitModule = module {
     single<GsonConverterFactory> {
         GsonConverterFactory.create(GsonBuilder().create())
     }
-    single {
-        HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
-            override fun log(message: String) {
-                Log.d("HttpLoggingInterceptor", message)
-            }
-        }).apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
-    }
-    single {
-        OkHttpClient.Builder().run {
-            val loggingInterceptor: HttpLoggingInterceptor = get()
-            addNetworkInterceptor(loggingInterceptor)
-            build()
-        }
-    }
     single<Retrofit> {
         val converterFactory: GsonConverterFactory = get()
+        val okHttpClient: OkHttpClient? = getOrNull()
         Retrofit.Builder()
             .baseUrl(BASE_OPEN_TRIVIA_URL)
             .addConverterFactory(converterFactory).apply {
-                if (BuildConfig.DEBUG) client(get()) // add logging interceptor
+                okHttpClient?.let(::client) // add logging interceptor
             }
             .build()
     }
