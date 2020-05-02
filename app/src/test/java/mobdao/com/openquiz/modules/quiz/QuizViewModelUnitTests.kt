5ff397647b92ec6875bf47aac1154dbefce84bbb
Diego Almeida de Oliveira <diegohkd@hotmail.com>
2020-05-02 15:45:35 -0300

    Refactor: unit tests

--- app/src/test/java/mobdao/com/openquiz/modules/quiz/QuizViewModelUnitTests.kt
+++ app/src/test/java/mobdao/com/openquiz/modules/quiz/QuizViewModelUnitTests.kt
@@ -2,11 +2,10 @@ package mobdao.com.openquiz.modules.quiz
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import androidx.navigation.NavDirections
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
-import io.mockk.verify
+import mobdao.com.openquiz.R
 import mobdao.com.openquiz.models.Game
 import mobdao.com.openquiz.models.Question
 import mobdao.com.openquiz.utils.pokos.ResultsReport
@@ -22,20 +21,27 @@ class QuizViewModelUnitTests {
 
     @MockK
     private lateinit var game: Game
+
     @MockK
     private lateinit var question1: Question
+
     @MockK
     private lateinit var question2: Question
+
     @MockK
     private lateinit var questionsObserver: Observer<List<Question>>
+
     @MockK
     private lateinit var confirmAnswerObserver: Observer<Unit>
+
     @MockK
     private lateinit var showCorrectAnswerObserver: Observer<Boolean>
+
     @MockK
     private lateinit var showNextQuestionObserver: Observer<Unit>
+
     @MockK
-    private lateinit var showResultsReportObserver: Observer<ResultsReport>
+    private lateinit var routeObserver: Observer<NavDirections>
 
     private val questions: List<Question> by lazy { listOf(question1, question2) }
 
@@ -49,9 +55,10 @@ class QuizViewModelUnitTests {
 
         quizViewModel.questionsLiveData.observeForever(questionsObserver)
         quizViewModel.getConfirmAnswerEvent(question1)?.observeForever(confirmAnswerObserver)
-        quizViewModel.getShowCorrectAnswerEvent(question1)?.observeForever(showCorrectAnswerObserver)
+        quizViewModel.getShowCorrectAnswerEvent(question1)
+            ?.observeForever(showCorrectAnswerObserver)
         quizViewModel.showNextQuestionEvent.observeForever(showNextQuestionObserver)
-        quizViewModel.showResultsReportEvent.observeForever(showResultsReportObserver)
+        quizViewModel.routeEvent.observeForever(routeObserver)
     }
 
     @Test
@@ -105,13 +112,18 @@ class QuizViewModelUnitTests {
         every { game.nextQuestion() }.returns(null)
         every { game.getNumberOfCorrectAnswers() }.returns(1)
         every { game.getNumberOfIncorrectAnswers() }.returns(0)
+        mockkObject(QuizFragmentDirections.Companion)
 
         quizViewModel.onNextClicked()
 
-        val resultsReport = quizViewModel.showResultsReportEvent.value
-        assertEquals(1, resultsReport?.correctAnswers)
-        assertEquals(0, resultsReport?.wrongAnswers)
-        verify { showResultsReportObserver.onChanged(any()) }
+        val slotResultsReport = slot<ResultsReport>()
+        verify { QuizFragmentDirections.Companion.toResultsReportFragment(capture(slotResultsReport)) }
+        val resultsReport = slotResultsReport.captured
+        val slotNavDirection = slot<NavDirections>()
+        verify { routeObserver.onChanged(capture(slotNavDirection)) }
+        assertEquals(1, resultsReport.correctAnswers)
+        assertEquals(0, resultsReport.wrongAnswers)
+        assertEquals(R.id.to_resultsReportFragment, slotNavDirection.captured.actionId)
     }
 
     @Test
